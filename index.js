b98958a2b133044be875a8d7fdfc837023be4d0c
miacastroco <miacastroco@unal.edu.co>
2019-06-15 16:31:33 -0500

    ldap passport

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
+const bodyParser   = require('body-parser');
 const app = express();
-var router = require("./routes/router");
-
 const cors = require("cors");
+var passport = require('passport');
+var LdapStrategy = require('passport-ldapauth').Strategy;
+var session = require('express-session');
+const { ldapURL } = require('./config/apiRoutes');
 
 require("./routes/index")(app);
 
-app.use(router);
 app.use(cors());
 
 app.use(function(req, res, next) {
@@ -17,6 +19,48 @@ app.use(function(req, res, next) {
   );
   next();
 });
+
+var OPTS = {
+  server: {
+    url: ldapURL,
+    bindDn: 'cn=admin',
+    bindCredentials: 'admin',
+    searchBase: 'dc=restaurants',
+    searchFilter: '(sAMAccountName={{username}})'
+  }
+};
+
+passport.use(new LdapStrategy(OPTS));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(session({
+  secret: 'ldap secret',
+  resave: false,
+  saveUninitialized: true,
+  cookie : { httpOnly: true, maxAge: 2419200000 } /// maxAge in milliseconds
+}));
+
+app.post('/login', passport.authenticate('ldapauth', {
+  successRedirect: '/', failureRedirect: '/login'
+}),
+  function(req, res) {
+    console.log(res);
+    res.send({status: 'ok'});
+  }
+); 
 
 const PORT = process.env.PORT || 5004;
 app.listen(PORT);
